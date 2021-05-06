72ea0eed00bc56cdec1f148f9724b9f5479bc076
Niamat <niamatullah.akbari@opp.eduvantaa.fi>
2021-05-06 14:20:45 +0300

    Nimatullah

--- src/findtheuknown/Story.java
+++ src/findtheuknown/Story.java
@@ -5,6 +5,7 @@ public class Story {
         System.out.println("We are the SEAL Team Six-Bravo. We are trying to execute a deadly ex cia member who was betrayed by his own people in government because he knew too many things, he went missing and now he is a thread to the nation");
 
         public void test(){
-            System.out.println("test");
+            System.out.println("Nimatullah");
         }
+
 }
