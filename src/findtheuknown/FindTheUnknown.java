59b1268a3a9046e6b54fa3607d3a13e0fef9bcd2
Zeta <83444918+zeta404@users.noreply.github.com>
2021-05-06 13:38:28 +0300

    Release 1.1

--- src/findtheuknown/FindTheUnknown.java
+++ src/findtheuknown/FindTheUnknown.java
@@ -5,7 +5,7 @@ package findtheuknown;
  *
  * @author masoo
  */
-public class FindTheUknown {
+public class FindTheUnknown {
 
     /**
      * @param args the command line arguments
@@ -15,7 +15,6 @@ public class FindTheUknown {
 
         char1 c1 = new char1("Russ Brown", "Male", 39, "US",87.03,1.90);
         char2 c2 = new char2("Dominique Giacinta", "Female", 31, "Mexico",86.03,1.52);
-        char2 c3 = new char2("Dominique Giacinta", "Female", 31, "Mexico",86.03,1.52);
         System.out.println(c1);
         System.out.println();
         System.out.println(c2);
