521ab63fd892a74dd3e3e2b16209670ab232503d
samuli404 <Samuli.karkkainen10@gmail.com>
2021-05-07 12:23:04 +0300

    updatededed

--- src/findtheunknown/Story.java
+++ src/findtheunknown/Story.java
@@ -7,95 +7,88 @@ public class Story {
 
 
 
-    Scanner sc = new Scanner(System.in);
-
     public static void StoryBegin() {
 
         System.out.println("Place: Norlisk ");
-        try
-        {
+        try {
             Thread.sleep(2000);
-        }
-        catch(InterruptedException ex)
-        {
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
         System.out.println("Time: 12.30pm");
 
-        try
-        {
+        System.out.println();
+
+        try {
             Thread.sleep(2000);
-        }
-        catch(InterruptedException ex)
-        {
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
 
         System.out.println("We are the SEAL Team Six-Bravo");
-        try
-        {
+        try {
             Thread.sleep(3000);
-        }
-        catch(InterruptedException ex)
-        {
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
 
         System.out.println("We are trying to execute a deadly ex cia member who was betrayed by his own people in government because he knew too many things.");
-        try
-        {
+        try {
             Thread.sleep(6000);
-        }
-        catch(InterruptedException ex)
-        {
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
         System.out.println("He went missing and now he is a thread to the nation");
 
-        try
-        {
-            Thread.sleep(3000);
-        }
-        catch(InterruptedException ex)
-        {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
+
+        System.out.println();
 
         System.out.println("SEAL team six-bravo is loading to the plane with 4 of the best SEAL members");
 
-        try
-        {
-            Thread.sleep(3000);
-        }
-        catch(InterruptedException ex)
-        {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
 
-        System.out.println("Be ready to jump, because its going to be a ruff way down.");
+        System.out.println("Be ready to jump, because its going to be a ruff way down!");
 
-        try
-        {
+        try {
             Thread.sleep(3000);
-        }
-        catch(InterruptedException ex)
-        {
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
+        System.out.println();
 
-        System.out.println("Press f to jump!");
-
-        try
-        {
+        try {
             Thread.sleep(1000);
-        }
-        catch(InterruptedException ex)
-        {
+        } catch (InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
+        Scanner sc = new Scanner(System.in);
+
+        String jumping = "f";
+
+        {
+            while (true) {
+                System.out.print("Press f to jump!");
+                String pf = sc.nextLine();
+
+                if (jumping.equals(pf)) {
+                    System.out.println("Great now you are in the skies dropping into the AO");
+                    break;
+                } else {
+                    System.out.println("Please press f exactly like how its written!");
 
 
+                }
 
+            }
+        }
     }
-
 }
