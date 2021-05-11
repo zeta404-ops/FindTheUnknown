09531382a2984c46d92740e1d54a71dae472d3a4
samuli404 <Samuli.karkkainen10@gmail.com>
2021-05-11 12:31:06 +0300

    kanava

--- src/findtheunknown/Sneak.java
+++ src/findtheunknown/Sneak.java
@@ -1,4 +1,20 @@
 package findtheunknown;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Sneak {
+
+    System.out.println("Okey lets start the plan!");
+
+    try {
+        Thread.sleep(3000);
+    } catch (InterruptedException ex) {
+        Thread.currentThread().interrupt();
+    }
+
+    System.out.println("Press m to open the map");
+
+    sc
 }
+
+
