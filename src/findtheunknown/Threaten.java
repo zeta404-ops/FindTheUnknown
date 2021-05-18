0f14f7056e14006d0b0ac25e7965b3e277eeed9c
samuli404 <Samuli.karkkainen10@gmail.com>
2021-05-18 10:26:57 +0300

    final

--- src/findtheunknown/Threaten.java
+++ src/findtheunknown/Threaten.java
@@ -21,7 +21,40 @@ public class Threaten {
             Thread.currentThread().interrupt();
         }
 
-        System.out.println("You need to aim to the bring the guards");
+        System.out.println("You need to aim the guard and bring him out the backdoor");
 
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("press x to aim the guard!");
+
+        String x = sc.nextLine();
+        if (x.equals(x)) {
+            System.out.println("Now you need to walk him out the backdoor");
+        }  else {
+        System.out.println("Please write the letter exactly like how its written!");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        System.out.println("press s to go slowly out the door with the guard!");
+            String s = sc.nextLine();
+            if (s.equals(s)) {
+                System.out.println("You escaped with the quard!");
+            }  else {
+                System.out.println("Please write the letter exactly like how its written!");
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+
+                System.out.println("Mission succesful!");
     }
 }
